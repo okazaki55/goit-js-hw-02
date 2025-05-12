84b909c32c56f7e67a6835ba1fd33d966daee23d
Furkan Toy <furkantoy@hotmail.com>
2025-05-12 10:13:55 +0300

    2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
-function formatMessage(message, maxLenght) {
-  if (message.lenght <= maxLenght) {
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
     return message;
   } else {
-    return message.slice(0, maxLenght) + '...';
+    return message.slice(0, maxLength) + '...';
   }
 }
 
